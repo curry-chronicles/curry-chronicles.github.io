0cd24ff57eac9b0a91489431ae7f3fa345a12750
Bastien G <bastien.guillon42@gmail.com>
2019-12-27 23:04:12 +0100

    wip add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 
+const RANDOM_INGREDIENTS = [
+	'un gros poulet',
+	'un gros steak',
+	'un gros couteau',
+	'beaucoup d\'oignons',
+	'du bœuf à la fraise',
+	'élégance',
+	'du pif',
+	'c\'est tout',
+	'Angular',
+	'Jean-Pierre Coffe',
+	'le fantôme de Jean-Pierre Coffe',
+	'Patrick Etchebest',
+	'ma maman',
+	'de la bière',
+	'beaucoup de bière',
+	'surtout de la bière'
+];
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -7,4 +26,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	public title = 'Curry Chronicles';
+	public randomIngredient: string;
+
+	constructor() {
+		this.generateRandomIngredient();
+	}
+
+	public generateRandomIngredient(): void {
+		this.randomIngredient = RANDOM_INGREDIENTS[Math.floor(Math.random() * RANDOM_INGREDIENTS.length)];
+	}
 }

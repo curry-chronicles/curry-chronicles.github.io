b301c55395c799c923703bdd22b5708066fcbe3e
Bastien G <bastien.guillon42@gmail.com>
2020-01-09 21:14:37 +0100

    complete schema

--- back/src/models/recipe.model.ts
+++ back/src/models/recipe.model.ts
@@ -10,10 +10,49 @@ export const RecipeSchema = mongoose.model(
 			id: String,
 			name: {
 				type: String,
+				default: 'DEFAULT'
+			},
+			mainPicture: {
+				type: String,
+				default: 'https://i.imgur.com/C9Fedsh.jpg'
+			},
+			headLine: {
+				type: String,
+				default: ''
+			},
+			servesHowManyPeople: {
+				type: Number,
+				default: 0
+			},
+			preparationTime: {
+				type: String,
+				default: '00:00:00'
+			},
+			cookingTime: {
+				type: String,
+				default: '00:00:00'
+			},
+			description: {
+				type: String,
 				default: 'Un délicieux DEFAULT'
 			},
-			mainPicture: String,
-			headLine: String
+			directions: [{
+				name: {
+					type: String,
+					default: 'Rien'
+				},
+				amount: {
+					type: Number,
+					default: 1
+				},
+				unit: String
+			}],
+			ingredients: [{
+				description: {
+					type: String,
+					default: 'Contemplez l\'existence.'
+				}
+			}]
 		},
 		{
 			collection: RecipeSchemaName

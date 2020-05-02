ba1e23d8a274d35ddcc046029d2698571dcccc70
bastienguillon <bastien.guillon42@gmail.com>
2020-05-02 20:33:40 +0200

    WIP upload to imgur

--- back/src/controllers/recipes.controller.ts
+++ back/src/controllers/recipes.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import * as imgur from 'imgur';
 import { Document } from 'mongoose';
-import { RecipeSchema } from '../models';
-import { AController } from './abstract.controller';
-import { Response as RequestResponse } from 'request';
 import * as httpRequest from 'request';
+import { Response as RequestResponse } from 'request';
+import { RecipeSchema } from '../models';
+import { imgurAuth } from './../imgur.json';
+import { AController } from './abstract.controller';
 import { LoginController } from './login.controller';
 
 export class RecipesController extends AController {
@@ -55,14 +57,27 @@ export class RecipesController extends AController {
 			response.send('Vous devez être identifié en tant qu\'admin');
 			return;
 		}
-		let newRecipe = new RecipeSchema(request.body);
-		newRecipe.save(
-			(error: Error, recipe: Document) => {
-				if (error) {
-					response.send(error);
-				}
-				response.json(recipe);
-			});
+
+		// TODO: remove the part before base64, at the beginning of the blob
+		const base64Picture = request.body.mainPicture as string;
+
+		imgur.setAPIUrl('https://api.imgur.com/3/');
+		imgur.setCredentials(imgurAuth.login, imgurAuth.password, imgurAuth.clientId);
+		imgur.uploadBase64(base64Picture, null, request.body.name, request.body.description)
+			.then(json => {
+				request.body.mainPicture = json.data.link;
+				const newRecipeSchema = new RecipeSchema(request.body);
+				newRecipeSchema.save(
+					(error: Error, recipe: Document) => {
+						if (error) {
+							response.send(error);
+						}
+						response.json(recipe);
+					});
+			})
+			.catch(error => {
+				response.send(error.message);
+			})
 	}
 
 	public delete(request: Request, response: Response): void {

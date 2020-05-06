238be542464b3c1e37327ad838e8a202a6492a21
bastienguillon <bastien.guillon42@gmail.com>
2020-05-06 23:15:46 +0200

    fix local links

--- front/src/app/ui/recipe/recipe.component.ts
+++ front/src/app/ui/recipe/recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
 import { IRecipe } from '../../models';
 
 @Component({
@@ -12,8 +13,16 @@ export class RecipeComponent {
 	public recipe: IRecipe;
 
 	constructor(
+		private router: Router,
 		private route: ActivatedRoute
 	) {
-		this.recipe = this.route.snapshot.data.recipe as IRecipe;
+		this.router.events.pipe(
+			filter(event => event instanceof NavigationEnd),
+			switchMap(() => this.route.data),
+			map(data => data?.recipe as IRecipe),
+			filter(recipe => recipe != null),
+		).subscribe(recipe => {
+			this.recipe = recipe;
+		});
 	}
 }

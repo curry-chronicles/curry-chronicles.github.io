064e9f1ae9ed4340275db10acf11682655a726a4
Bastien G <bguillon@lucca.fr>
2021-03-22 09:38:04 +0100

    use single quotes

--- front/src/app/pages/recipe-edition/recipe-edition.module.ts
+++ front/src/app/pages/recipe-edition/recipe-edition.module.ts
@@ -1,23 +1,23 @@
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from "@angular/material/button";
+import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { RouterModule, Routes } from "@angular/router";
-import { RecipeResolver, SharedModule } from "@curry-chronicles/shared";
-import { FormlyModule } from "@ngx-formly/core";
-import { FormlyMaterialModule } from "@ngx-formly/material";
-import { CURRY_CHRONICLES_FORMLY_CONFIG, DateComponent, DirectionsComponent, DirectionsEditorComponent, IngredientsComponent, IngredientsEditorComponent, PictureComponent, PictureEditorComponent, TimespanComponent, TimespanEditorComponent } from "./forms";
-import { RecipeEditionComponent } from "./recipe-edition.component";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeResolver, SharedModule } from '@curry-chronicles/shared';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { CURRY_CHRONICLES_FORMLY_CONFIG, DateComponent, DirectionsComponent, DirectionsEditorComponent, IngredientsComponent, IngredientsEditorComponent, PictureComponent, PictureEditorComponent, TimespanComponent, TimespanEditorComponent } from './forms';
+import { RecipeEditionComponent } from './recipe-edition.component';
 
 const routes: Routes = [
 	{
